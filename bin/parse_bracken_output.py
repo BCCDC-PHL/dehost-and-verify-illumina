4d9af36aedd89b4b309cb76c0875dc56274e5071
Dan Fornika <dfornika@gmail.com>
2020-08-19 17:43:43 -0700

    Add module for parsing bracken output

---
+++ bin/parse_bracken_output.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+
+import argparse
+import csv
+import json
+import sys
+
+
+def parse_bracken_output(path_to_bracken_output):
+    """
+    Args:
+        path_to_mlst_result (str): Path to the bracken output file.
+    Returns:
+        list of dict: Parsed bracken output.
+        For example:
+        [
+            {
+                'name': 'Homo sapiens',
+                'taxonomy_id': '9606',
+                'taxonomy_lvl': 'S',
+                'kraken_assigned_reads': 100,
+                'added_reads': 5,
+                'new_est_reads': 105,
+                'fraction_total_reads': 0.0052
+            },
+            ...
+        ]
+    """
+    bracken_output = []
+
+    int_fields = [
+        'kraken_assigned_reads',
+        'added_reads',
+        'new_est_reads',
+    ]
+
+    float_fields = [
+        'fraction_total_reads'
+    ]
+
+    with open(path_to_bracken_output) as bracken_output_file:
+        reader = csv.DictReader(bracken_output_file, delimiter='\t')
+        for row in reader:
+            for field in int_fields:
+                row[field] = int(row[field])
+            for field in float_fields:
+                row[field] = float(row[field])
+            bracken_output.append(row)
+
+    return bracken_output
+
+
+def calculate_total_reads(parsed_bracken_output):
+    total_reads = 0
+    for record in parsed_bracken_output:
+        total_reads += record['new_est_reads']
+
+    return total_reads
+
+
+def get_estimated_reads_by_name(parsed_bracken_output, name):
+    reads = 0
+    for record in parsed_bracken_output:
+        if record['name'] == name:
+            reads = record['new_est_reads']
+
+    return reads
+
+
+def main(args):
+    bracken_output = parse_bracken_output(args.bracken_output)
+
+    total_reads = calculate_total_reads(bracken_output)
+    host_reads = get_estimated_reads_by_name(bracken_output, args.host_name)
+    pathogen_reads = get_estimated_reads_by_name(bracken_output, args.pathogen_name)
+    other_reads = total_reads - host_reads - pathogen_reads
+
+    output = [
+        {
+            'pathogen_reads': pathogen_reads,
+            'host_reads': host_reads,
+            'other_reads': other_reads,
+        },
+    ]
+
+    output_fieldnames = [
+        'pathogen_reads',
+        'host_reads',
+        'other_reads',
+    ]
+    
+    writer = csv.DictWriter(sys.stdout, fieldnames=output_fieldnames, dialect='excel-tab')
+
+    writer.writeheader()
+    
+    for row in output:
+        writer.writerow(row)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host_name')
+    parser.add_argument('--pathogen_name')
+    parser.add_argument('bracken_output')
+    args = parser.parse_args()
+    main(args)
---
+++ modules/parse_bracken_output.nf
@@ -0,0 +1,18 @@
+process parse_bracken_output {
+    tag { sample_id }
+    executor 'local'
+    publishDir "${params.outdir}/pathogen_host_read_summary", pattern: "${sample_id}_pathogen_host_read_summary_${analysis_stage}.tsv", mode: 'copy'
+    input:
+    tuple val(sample_id), path(bracken_output), val(host_name), val(pathogen_name), val(analysis_stage)
+
+    output:
+    tuple val(sample_id), path("${sample_id}_pathogen_host_read_summary_${analysis_stage}.tsv")
+    
+    script:
+    """
+    parse_bracken_output.py \
+      --host_name "${host_name}" \
+      --pathogen_name "${pathogen_name}" \
+      ${bracken_output} > ${sample_id}_pathogen_host_read_summary_${analysis_stage}.tsv
+    """
+}
