703bf8a090b17898355fe417d7ee5110868dcf25
Dan Fornika <dfornika@gmail.com>
2020-08-21 16:47:18 -0700

    Create kraken2/bracken database

---
+++ .github/scripts/create_kraken2_and_bracken_database.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -eo pipefail
+
+echo Create kraken2 db .. >> artifacts/test_artifact.log
+
+# environment name is suffixed by long alphanumeric string
+# but it always starts with 'dehost-and-verify'. Use grep to find it.
+conda activate $(conda info --envs | grep dehost-and-verify | cut -d ' ' -f 1)
+
+KRAKEN2_DB_NAME=${HOME}/data/kraken2_bracken_db
+
+kraken2-build --db $KRAKEN2_DB_NAME --download-taxonomy
+kraken2-build --db $KRAKEN2_DB_NAME --download-library human --no-mask
+kraken2-build --db $KRAKEN2_DB_NAME --download-library viral
+kraken2-build --db $KRAKEN2_DB_NAME --build
+
+bracken-build -d $KRAKEN2_DB_NAME -l 150
+
+kraken2-build --db $KRAKEN2_DB_NAME --clean
+
