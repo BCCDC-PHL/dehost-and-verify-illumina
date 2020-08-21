9b75afa778f60ab911c3ff2d7b756957aa8b6f7d
Dan Fornika <dfornika@gmail.com>
2020-08-21 15:21:23 -0700

    Do not fail if directory exists

--- .github/scripts/download_hg38.sh
+++ .github/scripts/download_hg38.sh
@@ -3,6 +3,6 @@ set -eo pipefail
 
 echo Download hg38 .. >> artifacts/test_artifact.log
 
-mkdir /data
+mkdir -p /data
 wget -P /data https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/latest/hg38.fa.gz
 gunzip /data/hg38.fa.gz
