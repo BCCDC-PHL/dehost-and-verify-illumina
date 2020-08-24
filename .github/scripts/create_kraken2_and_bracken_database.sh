581c5b5b42d098012b7441f2ae97c149d199747c
Dan Fornika <dfornika@gmail.com>
2020-08-24 11:19:52 -0700

    Check that environment is being found

--- .github/scripts/create_kraken2_and_bracken_database.sh
+++ .github/scripts/create_kraken2_and_bracken_database.sh
@@ -5,6 +5,8 @@ echo Create kraken2 db .. >> artifacts/test_artifact.log
 
 cat ${HOME}/.bashrc >> artifacts/bashrc_artifact.txt
 
+echo env name $(conda info --envs | grep dehost-and-verify | cut -d ' ' -f 1) >> artifacts/test_artifact.log
+
 # environment name is suffixed by long alphanumeric string
 # but it always starts with 'dehost-and-verify'. Use grep to find it.
 source /opt/miniconda3/bin/activate $(conda info --envs | grep dehost-and-verify | cut -d ' ' -f 1)
