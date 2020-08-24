4bb547415c2762f8ce72b5654385d6eb0240ec7d
Dan Fornika <dfornika@gmail.com>
2020-08-24 11:49:34 -0700

    Fix path to artifacts logs

--- .github/scripts/set_up_conda_cache.sh
+++ .github/scripts/set_up_conda_cache.sh
@@ -9,29 +9,33 @@ export REPO=${PWD}
 echo REPO=${REPO} >> artifacts/test_artifact.log
 cd ..
 echo PWD=${PWD} >> ${REPO}/artifacts/test_artifact.log
+
 nextflow run ${REPO} \
        -profile conda \
        --cache ${HOME}/.conda/envs \
        --fastq_input .github/data/fastqs \
        --outdir output
-cp .nextflow.log artifacts/cache_creation.conda.profile.nextflow.log
+
+cp .nextflow.log ${REPO}/artifacts/cache_creation.conda.profile.nextflow.log
 
 cat .nextflow.log | grep 'Conda create complete env=/home/runner/work/dehost-and-verify-illumina/dehost-and-verify-illumina/environments/environment.yml path=/home/runner/work/.conda/envs/dehost-and-verify-' \
-    && echo "Conda env created in cache dir" >> artifacts/test_artifact.log \
+    && echo "Conda env created in cache dir" >> ${REPO}/artifacts/test_artifact.log \
 	|| bash -c "echo test failed\: Conda environment not created as expected >> artifacts/test_artifact.log && exit 1"
     
 rm -rf output && rm -rf work && rm -rf .nextflow*
+
 # second NF run will use the conda env created in the previous run
 echo re-run pipeline with conda --cache.. >> ${REPO}/artifacts/test_artifact.log
-NXF_VER=20.03.0-edge nextflow run ${REPO} \
+nextflow run ${REPO} \
        -profile conda \
        --cache ${HOME}/.conda/envs \
        --fastq_input .github/data/fastqs \
        --outdir output
-cp .nextflow.log artifacts/cache_use.conda.profile.nextflow.log
+
+cp .nextflow.log ${REPO}/artifacts/cache_use.conda.profile.nextflow.log
 
 cat .nextflow.log | grep 'Conda found local env for environment=/home/runner/work/dehost-and-verify-illumina/dehost-and-verify-illumina/environments/environment.yml; path=/home/runner/.conda/envs/dehost-and-verify-' \
-    && echo "Conda env found in cache dir" >> artifacts/test_artifact.log \
+    && echo "Conda env found in cache dir" >> ${REPO}/artifacts/test_artifact.log \
 	|| bash -c "echo test failed\: Conda environment not not found in cache dir >> artifacts/test_artifact.log && exit 1"
 
 # clean-up for following tests
