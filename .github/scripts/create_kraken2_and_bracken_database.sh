e0078453628984cc0a23a0c322cf40ac90f03d81
Dan Fornika <dfornika@gmail.com>
2020-08-24 17:50:47 -0700

    create kraken2 db directory

--- .github/scripts/create_kraken2_and_bracken_database.sh
+++ .github/scripts/create_kraken2_and_bracken_database.sh
@@ -13,7 +13,7 @@ source /opt/miniconda3/bin/activate $(conda info --envs | grep dehost-and-verify
 
 KRAKEN2_DB_NAME=${HOME}/data/kraken2_bracken_db
 
-mkdir $KRAKEN2_DB_NAME/taxonomy
+mkdir -p $KRAKEN2_DB_NAME/taxonomy
 rsync --no-motd rsync://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz $KRAKEN2_DB_NAME/taxonomy
 tar -C $KRAKEN_DB_NAME/taxonomy -xvf $KRAKEN2_DB_NAME/taxonomy/taxdump.tar.gz
 cp ${REPO}/.github/kraken2/nucl_gb_only_human_and_sars-cov-2.accession2taxid $KRAKEN2_DB_NAME/taxonomy
