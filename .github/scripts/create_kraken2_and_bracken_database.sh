24aff19d6e63bb9d085ef90f894f5f082c406813
Dan Fornika <dfornika@gmail.com>
2020-08-24 17:45:14 -0700

    Create limited kraken2 database

--- .github/scripts/create_kraken2_and_bracken_database.sh
+++ .github/scripts/create_kraken2_and_bracken_database.sh
@@ -3,6 +3,8 @@ set -eo pipefail
 
 echo Create kraken2 db .. >> artifacts/test_artifact.log
 
+export REPO=${PWD}
+
 echo env name $(/opt/miniconda3/bin/conda info --envs | grep 'dehost-and-verify' | cut -d ' ' -f 1) >> artifacts/test_artifact.log
 
 # environment name is suffixed by long alphanumeric string
@@ -11,10 +13,13 @@ source /opt/miniconda3/bin/activate $(conda info --envs | grep dehost-and-verify
 
 KRAKEN2_DB_NAME=${HOME}/data/kraken2_bracken_db
 
-kraken2-build --db $KRAKEN2_DB_NAME --download-taxonomy
+mkdir $KRAKEN2_DB_NAME/taxonomy
+rsync --no-motd rsync://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz $KRAKEN2_DB_NAME/taxonomy
+tar -C $KRAKEN_DB_NAME/taxonomy -xvf $KRAKEN2_DB_NAME/taxonomy/taxdump.tar.gz
+cp ${REPO}/.github/kraken2/nucl_gb_only_human_and_sars-cov-2.accession2taxid $KRAKEN2_DB_NAME/taxonomy
 kraken2-build --db $KRAKEN2_DB_NAME --download-library human --no-mask
-kraken2-build --db $KRAKEN2_DB_NAME --download-library viral
-kraken2-build --db $KRAKEN2_DB_NAME --build
+kraken2-build --db $KRAKEN2_DB_NAME --add-to-library ${REPO}/.github/data/kraken2/MN908947.3.kraken_annotated.fa
+kraken2-build --db $KRAKEN2_DB_NAME --build --max-db-size 1000000000
 
 bracken-build -d $KRAKEN2_DB_NAME -l 150
 
