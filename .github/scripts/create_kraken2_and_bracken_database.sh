9e06eea9c36f1f09051cbe5051419a9f039394dc
Dan Fornika <dfornika@gmail.com>
2020-08-25 09:45:05 -0700

    Log kraken2 db directory

--- .github/scripts/create_kraken2_and_bracken_database.sh
+++ .github/scripts/create_kraken2_and_bracken_database.sh
@@ -3,17 +3,18 @@ set -eo pipefail
 
 echo Create kraken2 db .. >> artifacts/test_artifact.log
 
-export REPO=${PWD}
+REPO=${PWD}
 
-echo env name $(/opt/miniconda3/bin/conda info --envs | grep 'dehost-and-verify' | cut -d ' ' -f 1) >> artifacts/test_artifact.log
+echo env name $(conda info --envs | grep 'dehost-and-verify' | cut -d ' ' -f 1) >> artifacts/test_artifact.log
 
 # environment name is suffixed by long alphanumeric string
 # but it always starts with 'dehost-and-verify'. Use grep to find it.
 source /opt/miniconda3/bin/activate $(conda info --envs | grep dehost-and-verify | cut -d ' ' -f 1)
 
 KRAKEN2_DB_NAME=${HOME}/data/kraken2_bracken_db
-
+echo KRAKEN2_DB_NAME=${KRAKEN2_DB_NAME} >> ${REPO}/artifacts/test_artifact.log
 mkdir -p $KRAKEN2_DB_NAME/taxonomy
+
 rsync --no-motd rsync://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz $KRAKEN2_DB_NAME/taxonomy
 tar -C $KRAKEN_DB_NAME/taxonomy -xvf $KRAKEN2_DB_NAME/taxonomy/taxdump.tar.gz
 cp ${REPO}/.github/kraken2/nucl_gb_only_human_and_sars-cov-2.accession2taxid $KRAKEN2_DB_NAME/taxonomy
