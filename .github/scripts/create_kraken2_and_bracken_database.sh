2e4bc473a18d5d33036d13d7eeacf1c09527ae4a
Dan Fornika <dfornika@gmail.com>
2020-08-25 09:51:12 -0700

    typo KRAKEN->KRAKEN2

--- .github/scripts/create_kraken2_and_bracken_database.sh
+++ .github/scripts/create_kraken2_and_bracken_database.sh
@@ -16,7 +16,7 @@ echo KRAKEN2_DB_NAME=${KRAKEN2_DB_NAME} >> ${REPO}/artifacts/test_artifact.log
 mkdir -p $KRAKEN2_DB_NAME/taxonomy
 
 rsync --no-motd rsync://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz $KRAKEN2_DB_NAME/taxonomy
-tar -C $KRAKEN_DB_NAME/taxonomy -xvf $KRAKEN2_DB_NAME/taxonomy/taxdump.tar.gz
+tar -C $KRAKEN2_DB_NAME/taxonomy -xvf $KRAKEN2_DB_NAME/taxonomy/taxdump.tar.gz
 cp ${REPO}/.github/kraken2/nucl_gb_only_human_and_sars-cov-2.accession2taxid $KRAKEN2_DB_NAME/taxonomy
 kraken2-build --db $KRAKEN2_DB_NAME --download-library human --no-mask
 kraken2-build --db $KRAKEN2_DB_NAME --add-to-library ${REPO}/.github/data/kraken2/MN908947.3.kraken_annotated.fa
