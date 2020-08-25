6b1ae8e146264833ceefc0802dcb0b7aa9b0f09a
Dan Fornika <dfornika@gmail.com>
2020-08-25 09:57:24 -0700

    Fix path to accession2taxid file

--- .github/scripts/create_kraken2_and_bracken_database.sh
+++ .github/scripts/create_kraken2_and_bracken_database.sh
@@ -17,7 +17,7 @@ mkdir -p $KRAKEN2_DB_NAME/taxonomy
 
 rsync --no-motd rsync://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz $KRAKEN2_DB_NAME/taxonomy
 tar -C $KRAKEN2_DB_NAME/taxonomy -xvf $KRAKEN2_DB_NAME/taxonomy/taxdump.tar.gz
-cp ${REPO}/.github/kraken2/nucl_gb_only_human_and_sars-cov-2.accession2taxid $KRAKEN2_DB_NAME/taxonomy
+cp ${REPO}/.github/data/kraken2/nucl_gb_only_human_and_sars-cov-2.accession2taxid $KRAKEN2_DB_NAME/taxonomy
 kraken2-build --db $KRAKEN2_DB_NAME --download-library human --no-mask
 kraken2-build --db $KRAKEN2_DB_NAME --add-to-library ${REPO}/.github/data/kraken2/MN908947.3.kraken_annotated.fa
 kraken2-build --db $KRAKEN2_DB_NAME --build --max-db-size 1000000000
